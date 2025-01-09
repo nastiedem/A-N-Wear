f4274da41ad40cca9093480731ee9398d2b19436
Vladimir Tryasunov <tryasunov933@gmail.com>
2025-01-09 10:43:24 +0200

    fix js and structure

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll("button");
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert(`You clicked on ${button.textContent}`);
-        });
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      alert(`You clicked on ${button.textContent}`);
     });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("a"); // Ищем все ссылки
+
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault(); // Отменяем стандартное поведение ссылки
+      const page = link.getAttribute("href"); // Получаем значение из href
+      if (page) {
+        window.location.href = page; // Выполняем переход
+      } else {
+        alert("Page URL is missing!");
+      }
+    });
+  });
 });
